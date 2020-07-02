bae9e6862e619e400423fe0e2a1e53a1a683fb8d
webmin41 <webmin41@gmail.com>
2020-07-02 11:44:49 +0900

    로직 수정  - 보드판 크기 변경  - 기본 종류 추가  - 게임 시작 시 처음부터 매칭 방지

--- game.js
+++ game.js
@@ -27,10 +27,11 @@ class Game {
         this.y_block = height / y_size;
         
         ctx.globalAlpha = .5;
-        ctx.strokeRect(0,0,width,height);
-        
+
         Array(x_size).fill(null).map((x,idx) => idx * this.x_block).forEach(x => ctx.fillRect(x, 0, 1, height));
         Array(y_size).fill(null).map((x,idx) => idx * this.y_block).forEach(x => ctx.fillRect(0, x, width, 1));
+        
+        ctx.strokeRect(0,0,width - 1,height - 1);
     }
 
     fillItem(x_size, y_size) {
@@ -48,14 +49,41 @@ class Game {
         const ctx = canvas.getContext('2d');
         ctx.globalAlpha = 1;
         ctx.font = "16pt Malgun Gothic"
+        ctx.strokeStyle = "#fff";
         this.items.forEach((x) => {
             const width = ctx.measureText(x.type).width;
+            ctx.fillStyle = x.background;
+            ctx.fillRect(x_block * x.coordinate.x, y_block * x.coordinate.y, x_block, y_block);
+            ctx.strokeRect(x_block * x.coordinate.x, y_block * x.coordinate.y, x_block, y_block);
+            ctx.fillStyle = "#fff";
             ctx.fillText(x.type, x_block * x.coordinate.x + x_block / 2 - width / 2, y_block * x.coordinate.y + y_block / 2 + 12);
         });
     }
 
     removeExploding() {
+        const {y_size} = this;
 
+        this.items.forEach((x,idx) => {
+            const type = x.type;
+
+            const [before_x, before_x_2, before_y, before_y_2] = [
+                this.items[idx - 1],
+                this.items[idx - 2],
+                this.items[idx - y_size],
+                this.items[idx - y_size * 2]
+            ];
+
+            if(!(before_x && before_x_2) || !(before_y && before_y_2)){
+                return true;
+            }
+
+            if(
+                (before_x.type === type && before_x_2.type === type) || 
+                (before_y.type === type && before_y_2.type === type)
+            ){
+                x.changeType();
+            }
+        })
     }
 }
 
@@ -63,7 +91,16 @@ class GameItem {
     type = null;
 
     constructor(x,y) {
-        this.type = ['사과','배','복숭아','키위'].sort(() => .5 - Math.random())[0];
+        this.types = [
+            {type: '1', background: '#111'},
+            {type: '2', background: '#222'},
+            {type: '3', background: '#333'},
+            {type: '4', background: '#444'},
+            {type: '5', background: '#555'},
+            {type: '6', background: '#666'},
+        ];
+
+        Object.assign(this, this.types[Math.floor(Math.random() * this.types.length)]);
         this.coordinate = {x,y};
     }
 
@@ -75,13 +112,18 @@ class GameItem {
         return 2;
     }
 
+    changeType() {
+        const valid = this.types.filter(x => x.type !== this.type);
+        Object.assign(this, valid[Math.floor(Math.random() * valid.length)]);
+    }
+
 }
 
 window.onload = function() {
     const width = 760,
           height = 760,
-          x_size = 10,
-          y_size = 10;
+          x_size = 12,
+          y_size = 12;
 
     const canvas = document.getElementById('game');
 
